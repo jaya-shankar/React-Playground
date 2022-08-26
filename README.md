f5993b922129807f91acfef72ca7cd5e1415fef5
jaya-shankar <bjayashankar2102@gmail.com>
2022-08-26 13:32:34 +0200

    code to replicate the bug

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # React-Playground
 To experiment react components &amp; libraries
+
+
+## Learning Resources
+- https://www.digitalocean.com/community/tutorials/react-axios-react
+- https://blog.openreplay.com/data-fetching-with-suspense-in-react
+- https://github.com/pmndrs/suspend-react
+
+
+## Tools
+- https://mockapi.io/
--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,76 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Button,Box,Text,
     PageContent,Main } from "grommet";
-import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
+import axios from "axios";
 
 
 
 export default function Content(){
 
     const [count,setCount] = React.useState(0);
-    const [showConfetti,setShowConfetti] = React.useState(false);
+    const [toggle,setToggle] = React.useState(false);
+    const [data,setData] = React.useState([]);
+    const [showLoading,setShowLoading] = React.useState(false);
+
 
     React.useEffect(() => {
-        if (count%3===0) {
-            // show confetti
-            setShowConfetti(true);
-            setTimeout(() => {
-                setShowConfetti(false);
+        
+        const fetchData = async () => {
+            let result = undefined;
+            if(toggle){
+                result = await axios(`https://63087357722029d9ddcf33e6.mockapi.io/transactions_v1`);
+                setToggle(false);
             }
-            ,5000);
-
+            else{
+                result = await axios(`https://63087357722029d9ddcf33e6.mockapi.io/transactions_v2`);
+                setToggle(true);
+                
+            }
+            setData(result.data);
         }
-    }, [count]);
 
-    const { width, height } = useWindowSize()
+        console.log("in effect hook")
+        fetchData();        
+        
+    }, [count]);
 
     function Increment(){
         setCount(count+1);
     }
+
     return (
     <PageContent>
         <Main pad="large">
-            <Text size="large">{count}</Text>
+            <Suspense fallback={<Box align="center" justify="center"><Text>Loading...</Text></Box>}>
+            {    (toggle)
+                    ?(
+                        <Text>
+                            {console.log("print only id")}
+                            {data.map(item => (
+                                <Box key={item.id}>
+                                    <Text>{item.id}</Text>
+                                </Box>
+                            ))}
+                        </Text>
+                    )
+                    :(
+                        <Text>
+                            {console.log("print id and name")}
+                            {data.map(item => (
+                                <Box key={item.id}>
+                                    <Text>{item.id} . {item.name}</Text>
+                                </Box>
+                            ))}
+                        
+                        </Text>
+                    )
+                
+            }
+            </Suspense>
             <Box pad="large" background="light-2">
                 <Button label="Click me" onClick={Increment}/>
             </Box>
         </Main>
-        {showConfetti && <Confetti width={width} height={height}/>}
     </PageContent>
     );
 };
